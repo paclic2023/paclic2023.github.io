87c2a813a138b081cc8aa012002817696f5c9268
pranav-ust <pagrawal@connect.ust.hk>
2024-01-28 22:02:24 +0800

    add accepted

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"A Roundtable for PhD Students and Young Scholars in Linguistics and Language Sciences Time: 11:00-12:30 am, 2 September 2023 Venue: The Hong Kong Polytechnic University Event Sponsored by: the RGC Collocated with: PACLIC 2023 (link) Panelists: Mary Dalrymple (Oxford University) Lori Levin (Carnegie Mellon University) Charles Yang (University of Pennsylvania)...","categories": ["blog"],
         "tags": ["announcement","publicity"],
         "url": "http://localhost:4000/blog/roundtable/",
+        "teaser":null},{
+        "title": "Workshop on South Asian Linguistics",
+        "excerpt":"Workshop Programme Date: 5 December 2023 Time: 9AM – 1PM Location: BC202, PolyU Schedule Time Presentation Presenter(s) 9:00-9:30 Introduction and opening remarks Mary Dalrymple, The Hong Kong Polytechnic University and University of Oxford 9:30-10:00 Variation in Nubri object marking Cathryn Donohue, The Hong Kong Polytechnic University 10:00-10:30 What do children...","categories": ["blog"],
+        "tags": ["announcement","publicity"],
+        "url": "http://localhost:4000/blog/workshop/",
         "teaser":null}]
